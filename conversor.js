2c37ebe539fba47879a742b366bd61f42aa9a101
lucivalluz <lucivalsouza@hotmail.com>
2023-10-26 22:14:56 -0300

    Conversor de moedas

--- conversor.js
+++ conversor.js
@@ -1,21 +1,21 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
-   
-    const inputCurrencyValue = document.querySelector(".input-currency").value 
+function convertValues() {
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
 
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
     const currencyValueConverted = document.querySelector(".currency-value") // Valor convertido Dolar, Euro, Libra etc
 
     console.log(currencySelect.value)
-    
+
     const dollarToday = 4.99
     const euroToday = 5.27
     const libraToday = 6.05
 
-    
-     if(currencySelect.value == "dolar") {
+
+    if (currencySelect.value == "dolar") {
 
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -23,42 +23,65 @@ function convertValues(){
         }).format(inputCurrencyValue / dollarToday)
 
 
-     }
+    }
 
-     if(currencySelect.value == "euro") {
+    if (currencySelect.value == "euro") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
             style: "currency",
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
 
-        
-     }
 
-     if(currencySelect.value == "libra") {
-currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
-    style: "currency",
-    currency: "GBP"
-}).format(inputCurrencyValue / libraToday)
+    }
 
-
-        
-     }
+    if (currencySelect.value == "libra") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
 
 
 
-
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-    
 
 
 
     console.log(convertValue)
+
+}
+
+function changeCurrency() {
+
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar americano"
+        currencyImage.src = "assets/dolar.png"
+    }
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "assets/euro.png"
+    }
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "assets/libra 1.png"
+    }
+
+    convertValues()
 }
 
 
+
+
+
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
