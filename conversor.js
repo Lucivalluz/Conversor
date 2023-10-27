18c50f620895aafbef928fed121cd7767638f8e1
lucivalluz <lucivalsouza@hotmail.com>
2023-10-26 21:12:21 -0300

    Conversor de moedas

--- conversor.js
+++ conversor.js
@@ -1,4 +1,5 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
 function convertValues(){
    
@@ -7,21 +8,52 @@ function convertValues(){
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert") // Valor em Real
     const currencyValueConverted = document.querySelector(".currency-value") // Valor convertido Dolar, Euro, Libra etc
 
-
-
+    console.log(currencySelect.value)
+    
     const dollarToday = 4.99
+    const euroToday = 5.27
+    const libraToday = 6.05
 
-    const convertValue = inputCurrencyValue / dollarToday
+    
+     if(currencySelect.value == "dolar") {
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dollarToday)
+
+
+     }
+
+     if(currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+
+        
+     }
+
+     if(currencySelect.value == "libra") {
+currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP"
+}).format(inputCurrencyValue / libraToday)
+
+
+        
+     }
+
+
+
+
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertValue)
+    
 
 
 
